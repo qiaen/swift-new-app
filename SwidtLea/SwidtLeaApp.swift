b3501be98cf8a3ae8a535f34bfb5d2fe56a5c608
qiaen <762366705@qq.com>
2026-08-13 07:53:08 +0800

    Initial Commit

---
+++ SwidtLea/SwidtLeaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwidtLeaApp.swift
+//  SwidtLea
+//
+//  Created by 刘洪亮 on 2026/8/13.
+//
+
+import SwiftUI
+
+@main
+struct SwidtLeaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
